0a92e7269307ba7acec0b00c5b11a3331f6dec46
root <root@debian.hardcoreanarchy.gay>
2025-11-03 13:40:34 -0800

    home files

--- move_files.sh
+++ move_files.sh
@@ -1,2 +1,5 @@
 cp /root/dwm/dwm-customized.patch /root/Linux-Setup/dwm/dwm-customized.patch
 cp /root/st/st-customized.patch /root/Linux-Setup/st/st-customized.patch
+
+cp /home/sebastian/.xinitrc /root/Linux-Setup/home/.xinitrc
+cp /home/sebastian/.config/gtk-3.0/settings.ini /root/Linux-Setup/home/.config/gtk-3.0/settings.ini
