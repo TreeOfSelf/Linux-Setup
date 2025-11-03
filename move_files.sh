345f0be3d770e81e515c61b4a7e7e52e55ca5d75
root <root@debian.hardcoreanarchy.gay>
2025-11-03 13:48:15 -0800

    capslock

--- move_files.sh
+++ move_files.sh
@@ -3,3 +3,4 @@ cp /root/st/st-customized.patch /root/Linux-Setup/st/st-customized.patch
 
 cp /home/sebastian/.xinitrc /root/Linux-Setup/home/.xinitrc
 cp /home/sebastian/.config/gtk-3.0/settings.ini /root/Linux-Setup/home/.config/gtk-3.0/settings.ini
+cp /home/sebastian/scripts/capslock-fix.sh /root/Linux-Setup/home/scripts/capslock-fix.sh
